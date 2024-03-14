ccc0bde8ba182480343a73fe9d41a07029aa7bc7
cuugithub <116214993+cuugithub@users.noreply.github.com>
2024-03-14 07:55:27 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("hehe", "may nghe nhac", "1234", 49.99f, 120);
+        Book book = new Book("vi cau la ban nho cua to", "sach chon", "5678", 29.99f, "Tool Pahm", 500, "Chon");
         mp3.showInfo();
-
-        Book book;
+        System.out.println();
         book.showInfo();
-
     }
 }
